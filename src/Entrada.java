8d9f66793468783b66d79f5222824afa95045f06
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-04-03 13:39:05 +0200

    update

--- src/Entrada.java
+++ src/Entrada.java
@@ -4,7 +4,7 @@ public class Entrada {
     public static void main(String[] args) {
         Coche coche1 = new Coche("Seat", "Ibiza", 75, 1400, "4562COR", 40, 100);
         coche1.mostrarDatos();
-        coche1.acelerar(10);
+        coche1.acelerar(5);
         coche1.mostrarDatos();
     }
 }
--- src/cochesCarrera/Carrera.java
+++ src/cochesCarrera/Carrera.java
@@ -1,4 +1,7 @@
 package cochesCarrera;
 
 public class Carrera {
+    private Object coche1;
+    private  Object coche2;
+    private
 }
